bbc1cf961edea26d560f9207571c3da55aa4984b
Aviv Brook <avivbrook@gmail.com>
2018-06-18 09:16:16 -0700

    Update ch5ex5

--- Intel-Assembly-Language-Programming-Exercises/5_5_BetterRandomRangeProcedure.asm
+++ Intel-Assembly-Language-Programming-Exercises/5_5_BetterRandomRangeProcedure.asm
@@ -12,8 +12,8 @@ N	DWORD	100
 .CODE
 main PROC
 	call	Clrscr
-	mov	eax,M
-	mov	ebx,N
+	mov	eax,N
+	mov	ebx,M
 	mov	ecx,NUMBER_OF_CALLS
 GenerateValues:
 	call BetterRandomRange
@@ -21,12 +21,15 @@ loop GenerateValues
 	exit
 main ENDP
 
+; This procedure is a improved version fo the RandomRange procedure.
+; Instead of generating a pseudorandom integer between 0 and N - 1, it
+; generates an integer between M ands N - 1
+; Receives: EBX = M (lower range), EAX = N (upper range)
+; Returns: EAX = psuedorandom value
 BetterRandomRange PROC USES eax ebx
-	sub	ebx,eax
-	xchg	ebx,eax
-	call	RandomRange
-	neg	ebx
 	sub	eax,ebx
+	call	RandomRange
+	add	eax,ebx
 	call	Writeint
 	call	Crlf
 	ret
---
+++ Intel-Assembly-Language-Programming-Exercises/5_6_RandomString.asm
@@ -0,0 +1,1 @@
+TITLE Random Strings
